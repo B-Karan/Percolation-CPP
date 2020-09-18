0e230ad78cecc5becaeed40eb7daa26f0b8ab95a
Karan <karan.bishwakarma1014@gmail.com>
2020-09-18 13:27:18 +0530

    Backwash problem solved and added comments

--- percolation.cpp
+++ percolation.cpp
@@ -1,3 +1,8 @@
+/*
+    Percolation modelled using NxN grid and Union-Find Datastructure
+
+    author: Karan Bishwakarma
+*/
 #include <iostream>
 #include <vector>
 #include "unionfind.h"
@@ -5,60 +10,75 @@
 class Percolation{
     int n;
     vector<vector<bool>> grid;
+
     UnionFind *uf;
+    UnionFind *ufForFullness;
+
     int openSites;
+    int virtualTop;
+    int virtualBottom;
 
 public:
     Percolation(int N){
         n = N;
         grid.assign(N, vector<bool>(N, false));
+
         uf = new UnionFind(N*N + 2);
+        ufForFullness = new UnionFind(N*N + 1);
+
         openSites = 0;
+        virtualTop = 0;
+        virtualBottom = N*N + 1;
 
         for(int i = 0; i < N; ++i){
-            uf->_union(0, encode(0, i));
-            uf->_union(N*N + 1, encode(N-1, i));
+            uf->_union(virtualTop, encode(0, i));
+            uf->_union(virtualBottom, encode(N-1, i));
+
+            ufForFullness->_union(virtualTop, encode(0, i));
         }
     }
 
+    // Opening a
     void open(int row, int column){
         if(isOpen(row, column)) return;
-        if(row < 0 || row > n-1 || column < 0 || column > n-1) return;
+
         grid[row][column] = true;
-        openSites++;
-        if(row-1 >=0 && row-1 < n && isOpen(row-1, column)){
-            uf->_union(encode(row-1, column), encode(row, column));
+        ++openSites;
+
+        // Connecting to left neighbor
+        if(column > 0 && isOpen(row, column - 1)){
+            uf->_union(encode(row, column), encode(row, column - 1));
+            ufForFullness->_union(encode(row, column), encode(row, column - 1));
         }
-        if(row+1 >= 0 && row+1 < n && isOpen(row+1, column)){
-            uf->_union(encode(row+1, column), encode(row, column));
+        // Connecting to right neighbor
+        if(column < (n-1) && isOpen(row, column + 1)){
+            uf->_union(encode(row, column), encode(row, column + 1));
+            ufForFullness->_union(encode(row, column), encode(row, column + 1));
         }
-        if(column-1 >= 0 && column-1 < n && isOpen(row, column-1)){
-            uf->_union(encode(row, column-1), encode(row, column));
+        // Connecting to top neighbor
+        if(row > 0 && isOpen(row - 1, column)){
+            uf->_union(encode(row, column), encode(row - 1, column));
+            ufForFullness->_union(encode(row, column), encode(row - 1, column));
         }
-        if(column+1 >= 0 && column+1 < n && isOpen(row, column+1)){
-            uf->_union(encode(row, column+1), encode(row, column));
+        // Connecting to bottom neighbor
+        if(row < (n-1) && isOpen(row + 1, column)){
+            uf->_union(encode(row, column), encode(row + 1, column));
+            ufForFullness->_union(encode(row, column), encode(row + 1, column));
         }
     }
-
+    // If site is Open
     bool isOpen(int row, int column){ return grid[row][column]; }
 
-    bool isFull(int row, int column){ return isOpen(row, column) && uf->connected(0, encode(row, column)); }
+    // If site if Full
+    bool isFull(int row, int column){ return isOpen(row, column) && ufForFullness->connected(virtualTop, encode(row, column)); }
 
-    bool percolates(){ return uf->connected(0, n*n +1); }
+    // If grid percolates
+    bool percolates(){ return uf->connected(virtualTop, virtualBottom); }
 
+    // Returns number of open sites
     int numberOfOpenSites(){ return openSites; }
 
-    void printGrid(){
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; ++j){
-                cout << grid[i][j] << " ";
-            }
-        cout << endl;
-        }
-    cout << endl;
-    uf->printId();
-    }
-
 private:
+    // Returns index for given row and column
     int encode(int row, int column){ return(n * row + column + 1); }
 };
